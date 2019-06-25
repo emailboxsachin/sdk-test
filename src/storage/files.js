4b3f24d16171e34cf42c60afbf7acdb75b3a176f
Sachin Gaikwad <emailbox.sachin@rediffmail.com>
2019-06-25 15:23:39 +0530

    Update files.js

--- src/storage/files.js
+++ src/storage/files.js
@@ -1,6 +1,6 @@
 'use strict';
 /*jshint -W079 */
-var Promise = require('bluebird');
+/*var Promise = require('bluebird');
 var r = require('hg-base').RestConnector;
 const https = require('https');
 
@@ -280,4 +280,41 @@ Files.prototype.postProcessAgencyFiles = function(fileData, req) {
     });
 };
 
+
 module.exports = Files;
+*/
+var AWS = require('aws-sdk');
+AWS.config.update({region: process.env.REGION});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({
+    apiVersion: process.env.APIVERSION,
+    endpoint: process.env.ENDPOINT + ':' + process.env.SQSPORT
+});
+
+function SQSMessaging() {
+    var self = this;    
+}
+
+SQSMessaging.prototype.createQueue = function(){
+    var params = {
+        QueueName: process.env.QUEUENAME,
+        Attributes: {
+          'DelaySeconds': process.env.DELAYSECONDS,
+          'MessageRetentionPeriod': process.env.MESSAGERETENTIONPERIOD
+        }
+    }; 
+    return new Promise(function(fulfill, reject) {
+        sqs.createQueue(params, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+                reject(err);
+            } else {
+                fulfill(data);
+                console.log("Success", data.QueueUrl);
+            }
+        });
+    })
+}
+
+module.exports = SQSMessaging;
